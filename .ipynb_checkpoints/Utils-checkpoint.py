e5e5a12dd7c2100c96b09ca54016d012de85830a
SeyedShobeiri <shobeiri@math.uh.edu>
2021-10-05 11:27:30 -0500

    continued on classification

--- .ipynb_checkpoints/Utils-checkpoint.py
+++ .ipynb_checkpoints/Utils-checkpoint.py
@@ -4,6 +4,11 @@ import numpy as np
 from zlib import crc32
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
 proj_root_path = '/Users/homayoonshobeiri/Desktop/Python for Github/Data Science ShowCase for Recruiters/Data-Science/Utils'
 images_path = os.path.join(proj_root_path,"Images")
 
@@ -72,3 +77,25 @@ def disply_cross_val_info(scores):
     print("Scores:",scores)
     print("Mean:",scores.mean())
     print("Standard Deviation:",scores.std())
+    
+def plot_digit(data):
+    image = data.reshape(28,28)
+    plt.imshow(image,cmap = mpl.cm.binary,
+               interpolation = "nearest")
+    plt.axis("off")
+    
+    
+def plot_digits(instances,images_per_row = 10, **options):
+    size = 28
+    images_per_row = min(len(instances),images_per_row)
+    images = [instance.reshape(size,size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size,size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap = mpl.cm.binary, **options)
+    plt.axis("off")
